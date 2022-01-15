b1f55f9e871142d949d9abf30cb578d91befd6d4
Tarcisio Paixão <tarcisio.santostj@hotmail.com>
2022-01-15 10:15:57 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/ScorePK.java
@@ -0,0 +1,40 @@
+package com.devsuperior.dsmovie.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class ScorePK implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@ManyToOne
+	@JoinColumn(name = "tb_movie")
+	private Movie movie;
+	
+	@ManyToOne
+	@JoinColumn(name = "tb_user")
+	private User user;
+	
+	public ScorePK(){
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+}
